22ea68ffe42dc203314bc4a518e84855aab3e425
PaulStoffregen <paul@pjrc.com>
2020-05-14 11:00:56 -0700

    Add Teensy4_PinList example

---
+++ examples/Teensy4_PinList/Teensy4_PinList.ino
@@ -0,0 +1,78 @@
+/*  OctoWS2811 Teensy4_PinList.ino - Demonstrate use of any pins
+    http://www.pjrc.com/teensy/td_libs_OctoWS2811.html
+
+  With Teensy 4.x, you can use any group of pins.  You are not limited
+  to only 8 fixed pins as with Teensy 3.x.  This example shows how to
+  use only 4 pins, which are the 4 pins of the Octo28 shield which do
+  not conflict with the Audio shield.
+
+  Required Connections
+  --------------------
+    pin 2:  LED Strip #1
+    pin 14: LED strip #2
+    pin 6:  LED strip #5
+    pin 5:  LED strip #8
+*/
+
+#include <OctoWS2811.h>
+
+// Any group of digital pins may be used
+const int numPins = 4;
+byte pinList[numPins] = {2, 14, 6, 5};
+
+const int ledsPerStrip = 120;
+
+DMAMEM int displayMemory[ledsPerStrip*6];
+int drawingMemory[ledsPerStrip*6];
+
+const int config = WS2811_GRB | WS2811_800kHz;
+
+OctoWS2811 leds(ledsPerStrip, displayMemory, drawingMemory, config, numPins, pinList);
+
+void setup() {
+  leds.begin();
+  leds.show();
+}
+
+#define RED    0xFF0000
+#define GREEN  0x00FF00
+#define BLUE   0x0000FF
+#define YELLOW 0xFFFF00
+#define PINK   0xFF1088
+#define ORANGE 0xE05800
+#define WHITE  0xFFFFFF
+
+// Less intense...
+/*
+#define RED    0x160000
+#define GREEN  0x001600
+#define BLUE   0x000016
+#define YELLOW 0x101400
+#define PINK   0x120009
+#define ORANGE 0x100400
+#define WHITE  0x101010
+*/
+
+void loop() {
+  int microsec = 2000000 / leds.numPixels();  // change them all in 2 seconds
+
+  // uncomment for voltage controlled speed
+  // millisec = analogRead(A9) / 40;
+
+  colorWipe(RED, microsec);
+  colorWipe(GREEN, microsec);
+  colorWipe(BLUE, microsec);
+  colorWipe(YELLOW, microsec);
+  colorWipe(PINK, microsec);
+  colorWipe(ORANGE, microsec);
+  colorWipe(WHITE, microsec);
+}
+
+void colorWipe(int color, int wait)
+{
+  for (int i=0; i < leds.numPixels(); i++) {
+    leds.setPixel(i, color);
+    leds.show();
+    delayMicroseconds(wait);
+  }
+}
